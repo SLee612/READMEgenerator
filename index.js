411078ad7454c208470174b9a95b1b50b6a3622f
SLee612 <sarahlee612@gmail.com>
2021-04-01 08:51:27 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-# **README Generator**
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-## description
-This is a simple README generator.  Users are asked a series of questions that are then dropped into a "template".
+const generateRM = (answers) =>
+
+`# **${answers.name}**
+
+## Description
+${answers.motivation}
 
 ## Table of Contents
 I. [Installation](#Installation)
@@ -19,14 +24,14 @@ VI. [Questions](#Questions)
 ***
 
 ## I. "Installation"
-Run node index.js in the terminal to install.
+${answers.installation}
 
 ## II. Usage
-N/A
+${answers.usage}
 
 ## III. License
 [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
-Copyright 2021 Sarah Lee 
+Copyright 2021 ${answers.license} 
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
@@ -39,10 +44,63 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTH
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ## IV. Contributing
-undefined
+${answers.contributing}
 
 ## V. Tests
-N/A
+${answers.tests}
 
 ## VI. Questions
-github.com/slee
+github.com/${answers.questions}`
+
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Project Title-',
+    },
+    {
+      type: 'input',
+      name: 'motivation',
+      message: 'Provide a brief description of your project-',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Installation instructions-',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Usage information-',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Your Full Name-',
+      },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Test Instructions-',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'GitHub Username-',
+      },
+  ])
+  .then((answers) => {
+    const rmPageContent = generateRM(answers);
+
+    fs.writeFile('README.md', rmPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
